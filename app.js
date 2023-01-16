5d9cc2fe133d0edbaf3f1dd983636c213259ad0b
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2023-01-16 16:42:51 +0400

    add config

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+require("dotenv").config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const connectDB = require("./config/dbConn")
+const mongoose = require("mongoose")
+const cors = require('cors')
+const corsOption = require("./config/corsOptions")
+const PORT = process.env.PORT || 3500
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -13,11 +18,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+connectDB()
+
 app.use(logger('dev'));
+app.use(cors(corsOption))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -27,15 +38,24 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err.stack)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  const status = res.statusCode ? res.statusCode : 500 // server error
+  res.status(status);
+
+  res.json({ message: err.message, isError: true })
 });
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB")
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+})
+mongoose.connection.on("error", err => {
+  console.log(err)
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
 
 module.exports = app;
